8e85a934a0a6d34ebe17bdc4f854f3e25bb0723e
Lukaz1802 <lukaz1802.ll@gmail.com>
2021-07-13 15:14:28 -0300

    Agrego tarea

---
+++ Tarea/Tarea 14-07/HelloWorld app-react/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+import "./index.css";
+import HelloWorld from "./HelloWorld/HelloWorld";
+
+
+
+ReactDOM.render(
+
+  <HelloWorld />,
+  document.getElementById("root")
+);

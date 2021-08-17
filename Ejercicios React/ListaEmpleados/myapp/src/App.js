bdb96c6167762ea5416d1ead46ebd57c37b69615
Lukaz1802 <lukaz1802.ll@gmail.com>
2021-08-17 03:31:39 -0300

    Agrego ejercicio ListaEmpleados React

---
+++ Ejercicios React/ListaEmpleados/myapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import EmpleadoList from './components/EmpleadoList';
+
+function App() {
+  return (
+    <div className="App">
+      <EmpleadoList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Ejercicios React/ListaEmpleados/myapp/src/components/EmpleadoList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import EmpleadoRow from './EmpleadoRow'
+
+
+const EmpleadoList = () => {
+    return (
+        <div>
+            <EmpleadoRow empleados={empleados} />
+        </div>
+    )
+}
+
+export default EmpleadoList
